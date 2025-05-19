3271b20a3c64f83709c85ab63afb9b17e2a24c32
Brian Amedro <brian@amedro.fr>
2025-05-19 10:51:51 +0000

    setup tfenv

--- main.tf
+++ main.tf
@@ -1,12 +1,6 @@
      terraform {
-       backend "remote" {
+       backend "local" {
          # The name of your Terraform Cloud organization.
-         organization = "example-organization"
-
-         # The name of the Terraform Cloud workspace to store Terraform state files in.
-         workspaces {
-           name = "example-workspace"
-         }
        }
      }
 
